9c431730a6a5fff47b133897b37aa4ef236f038f
Justin Harris <justin.harris@wpengine.com>
2015-08-16 22:24:30 -0500

    Adding circle drawing

--- commands.py
+++ commands.py
@@ -1,4 +1,5 @@
-from kivy.graphics import Color, Ellipse, Line
+from kivy.graphics import Ellipse, Line
+from kivy.vector import Vector
 
 
 class LinePreview:
@@ -25,6 +26,32 @@ class LinePreview:
         return LineCommand(self.canvas, self.start_coords, self.end_coords)
 
 
+class CirclePreview:
+
+    def __init__(self, canvas):
+        self.canvas = canvas
+
+    def start(self, x, y):
+        self.center = (x, y)
+
+        with self.canvas:
+            self.circle = Ellipse(pos=self.center, size=(0, 0))
+
+    def move(self, x, y):
+        radius = Vector(self.center).distance((x, y))
+        self.circle.pos = (self.center[0] - radius, self.center[1] - radius)
+        self.circle.size = (radius * 2, radius * 2)
+
+    def stop(self, x, y):
+        self.radius = Vector(self.center).distance((x, y))
+        self.canvas.remove(self.circle)
+        self.circle = None
+
+    @property
+    def command(self):
+        return CircleCommand(self.canvas, self.center, self.radius)
+
+
 # Note that the canvas is the receiver in this scenario
 class LineCommand:
 
@@ -43,4 +70,24 @@ class LineCommand:
     def __str__(self):
         points = [int(point) for point in (self.start + self.end)]
         return 'Line:  ({}, {}) -> ({}, {})'.format(*points)
+
+class CircleCommand:
+
+    def __init__(self, canvas, center, radius):
+        self.canvas = canvas
+        self.center = center
+        self.radius = radius
+
+    def execute(self):
+        with self.canvas:
+            self.circle = Ellipse(
+                pos=(self.center[0] - self.radius, self.center[1] - self.radius),
+                size=(self.radius * 2, self.radius * 2)
+            )
+
+    def undo(self):
+        self.canvas.remove(self.circle)
+
+    def __str__(self):
+        return 'Circle:  ({}, {})  r: {}'.format(int(self.center[0]), int(self.center[1]), self.radius)
 
--- paint.py
+++ paint.py
@@ -3,7 +3,8 @@ from kivy.clock import Clock
 from kivy.uix.widget import Widget
 from kivy.uix.listview import ListView
 from kivy.properties import ObjectProperty
-from commands import LinePreview
+
+import commands
 
 
 class PaintHistory(ListView):
@@ -48,13 +49,14 @@ class PaintCanvas(Widget):
         super(PaintCanvas, self).__init__(*args, **kwargs)
 
         self.preview = None
+        self.preview_class = 'LinePreview'
 
     def clear(self):
         self.canvas.clear()
 
     def on_touch_down(self, touch):
         if self.collide_point(touch.x, touch.y):
-            self.preview = LinePreview(self.canvas)
+            self.preview = self.preview_class(self.canvas)
             self.preview.start(touch.x, touch.y)
 
     def on_touch_move(self, touch):
@@ -67,6 +69,14 @@ class PaintCanvas(Widget):
             self.paint_history.add(self.preview.command)
 
             self.preview = None
+
+    @property
+    def preview_class(self):
+        return self._preview_class
+
+    @preview_class.setter
+    def preview_class(self, preview):
+        self._preview_class = getattr(commands, preview)
 
 
 class PaintApp(App):
